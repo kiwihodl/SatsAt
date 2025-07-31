7f01bfcbfbbaa0b23de4152635e69020a64b1b78
kiwihodl <orangeamber@protonmail.com>
2025-07-31 10:46:50 -0500

    Initial Commit

---
+++ Satsat/SatsatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatsatApp.swift
+//  Satsat
+//
+//  Created by Kiwi_ on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SatsatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SatsatTests/SatsatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SatsatTests.swift
+//  SatsatTests
+//
+//  Created by Kiwi_ on 7/31/25.
+//
+
+import Testing
+@testable import Satsat
+
+struct SatsatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
